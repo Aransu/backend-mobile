1488a9cee795f62731be4fb16bfe26b57eab3f21
Tramy21522352 <nttm.04062003@gmail.com>
2024-06-01 00:56:12 +0700

    done get and cancel trip(reservation)

--- routes/Booking.route.js
+++ routes/Booking.route.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { booking } from "../controllers/Booking.controller.js";
+import { booking, cancelTrip, getTrip } from "../controllers/Booking.controller.js";
 import { checkPermission } from "../middleware/checkPermission.js";
 
 const router = express.Router();
 
 router.post('/bookRoom', checkPermission, booking)
-
+router.get('/getTrip', checkPermission, getTrip)
+router.delete('/cancelTrip', checkPermission, cancelTrip)
 export default router;
--- services/Booking.service.js
+++ services/Booking.service.js
@@ -1,6 +1,7 @@
 import reservations from "../models/reservations.model.js";
 import User from '../models/user.model.js'
 import Room from '../models/room.model.js'
+import roomModel from "../models/room.model.js";
 const bookingService = async (req, res) => {
     try {
         const { user_id, room_id, start_date, end_date } = req.body;
@@ -18,12 +19,11 @@ const bookingService = async (req, res) => {
         const millisecondsDiff = endDate.getTime() - startDate.getTime();
         const daysDiff = Math.floor(millisecondsDiff / (1000 * 60 * 60 * 24));
         const createdAt = Date.now();
-        const user = await User.findOne({ _id: user_id })
         const room = await Room.findOne({ _id: room_id })
         const total = daysDiff * room.price;
         const newReservations = new reservations({
-            user,
-            room,
+            user_id,
+            room_id,
             start_date: startDate,
             end_date: endDate,
             total,
@@ -42,4 +42,73 @@ const bookingService = async (req, res) => {
     }
 }
 
-export { bookingService };
+const getTripService = async (req, res) => {
+    try {
+        const { user_id } = req.query;
+        if (!user_id) {
+            return {
+                status: 400,
+                message: "Missing fields"
+            };
+        }
+
+        const trip = await reservations.find({ user_id: user_id });
+        const updatedTrip = await Promise.all(trip.map(async e => {
+            const room = await roomModel.findOne({ _id: e.room_id });
+            return { ...e.toObject(), room };
+        }));
+        if (!trip || trip.length === 0) {
+            return {
+                status: 200,
+                message: "Trip is empty",
+                data: []
+            };
+        }
+
+        return {
+            status: 200,
+            message: "SUCCESS",
+            data: updatedTrip
+        };
+    } catch (err) {
+        return {
+            status: 500,
+            message: "An error occurred",
+            error: err.message
+        };
+    }
+};
+
+
+const cancelTripService = async (req, res) => {
+    try {
+        const { trip_id } = req.query;
+        if (!trip_id) {
+            return {
+                status: 400,
+                message: "Missing trip_id"
+            };
+        }
+
+        const trip = await reservations.findOneAndDelete({ _id: trip_id });
+        if (!trip) {
+            return {
+                status: 404,
+                message: "Trip not found"
+            };
+        }
+
+        return {
+            status: 200,
+            message: "Trip cancelled successfully",
+            data: trip
+        };
+    } catch (err) {
+        return {
+            status: 500,
+            message: "An error occurred",
+            error: err.message
+        };
+    }
+};
+export { bookingService, getTripService, cancelTripService };
